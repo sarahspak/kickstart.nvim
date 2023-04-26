351df1187d631487fb309fd625aacc2ce85da710
Pak <sarahpak@amazon.com>
2023-04-26 01:33:29 -0400

    added copilot to init.lua in the lazy.nvim setup

--- init.lua
+++ init.lua
@@ -65,7 +65,7 @@ vim.opt.rtp:prepend(lazypath)
 --    as they will be available in your neovim runtime.
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
-
+  'github/copilot.vim',
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
