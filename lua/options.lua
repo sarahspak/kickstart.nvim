f5b277e38ed4bce83a80745654cec935445ef9a0
Sarah Pak <sarahpak@amazon.com>
2023-11-21 03:48:06 +0000

    added even more options for vim

--- lua/options.lua
+++ lua/options.lua
@@ -3,3 +3,8 @@ vim.o.number = true
 vim.o.relativenumber = true
 -- highlight search
 vim.opt.hlsearch = true
+
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.expandtab = true
